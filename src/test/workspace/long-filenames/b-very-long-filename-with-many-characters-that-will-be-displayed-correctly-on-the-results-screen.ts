a6573d14c5639c477ff3f7f2123c41093d0e7657
Ole Kristian Fjaerestad <fjaerestad.olekristian@gmail.com>
2025-05-01 13:01:37 +0200

    Issue 19: Display import direction on results screen

---
+++ src/test/workspace/long-filenames/b-very-long-filename-with-many-characters-that-will-be-displayed-correctly-on-the-results-screen.ts
@@ -0,0 +1,5 @@
+import { a } from "./a-very-long-filename-with-many-characters-that-will-be-displayed-correctly-on-the-results-screen";
+
+export function b() {
+  console.info('b() calls a', a());
+}
